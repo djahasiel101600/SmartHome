fa2d2eaad1db200f3e33aca7a4f5c747e37ed005
djahasiel101600 <djahasiel101600@gmail.com>
2026-07-24 15:57:36 +0800

    update docker file

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from .views import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,4 +8,5 @@ urlpatterns = [
     path("api/", include("apps.devices.urls")),
     path("api/", include("apps.schedules.urls")),
     path("api/", include("apps.monitoring.urls")),
+    path('health/', health_check)
 ]
